f601fad1b28186dee07cfd8332519495a9e618dc
Grey16 <ssyyhh2012@gmail.com>
2017-07-06 15:35:00 -0400

    created event page script

---
+++ eventPage.js
@@ -0,0 +1,4 @@
+/*This is the script for the extension's event page.
+*It listens for tab updates, and redirects users to the default page when they try to go to another website.
+*It listens for when the active tab is switched, and redirects users to the default page when they switch into a blacklisted active tab
+*/

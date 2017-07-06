74c4c529706e018ae46e18c1cfc58e3001da9d90
Grey16 <ssyyhh2012@gmail.com>
2017-07-06 16:30:49 -0400

    added event listeners for redirecting users

--- eventPage.js
+++ eventPage.js
@@ -2,3 +2,17 @@
 *It listens for tab updates, and redirects users to the default page when they try to go to another website.
 *It listens for when the active tab is switched, and redirects users to the default page when they switch into a blacklisted active tab
 */
+
+var resetProp = {url: "chrome://extensions/"};
+var queryProp = {active: true, lastFocusedWindow: true, status: "complete"};
+
+
+chrome.tabs.onActivated.addListener(function() {
+	chrome.tabs.update(resetProp);
+});
+
+
+chrome.tabs.query(queryProp, function(tabs) {
+	chrome.tabs.update(tabs[0].id, resetProp);
+});
+

5b07fac91755e5613a73a2e7a14867b1ccd4fb8c
Grey16 <ssyyhh2012@gmail.com>
2017-07-20 11:29:10 -0400

    starting options page from scratch

--- options.js
+++ options.js
@@ -5,28 +5,7 @@ function setDefaults() {
 	document.getElementById('date').valueAsDate = new Date();
 }
 
-function setAlarm() {
-	//Get value in time and date
-	var when = document.getElementById('time').value;
-	document.getElementById('test').innerHTML = when.toString();
-	chrome.storage.sync.set({'time': when}, function() {
-		message('Settings saved.')
-	});
-	//Convert time and date value into seconds after epoch
-	
-	//Create alarm corresponding to the set time
-}
-
-function getTime() {
-	chrome.storage.sync.get('time', function(items) {
-		document.getElementById('test').innerHTML = items.time.toString();
-	});
-}
-
+//Sets the date picker to the current date upon page load
 window.addEventListener("load", setDefaults);
-var submit = document.getElementById('submit');
-submit.addEventListener("click", setAlarm);
-var button = document.getElementById('button');
-button.addEventListener("click", getTime);
-	
+
 

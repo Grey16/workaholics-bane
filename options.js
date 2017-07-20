ed9118c01290801fbfb8ef4cdc3666e40aa0084d
Grey16 <ssyyhh2012@gmail.com>
2017-07-20 11:57:59 -0400

    moved scripts tag to end of body, storage is working

--- options.js
+++ options.js
@@ -1,11 +1,29 @@
 /*This is the script for the options page.
 */
 
+//Sets the date picker to the current date upon page load
 function setDefaults() {
 	document.getElementById('date').valueAsDate = new Date();
+	chrome.storage.sync.get('time', function(items) {
+		document.getElementById('status').textContent = items.time;
+	});
 }
 
-//Sets the date picker to the current date upon page load
-window.addEventListener("load", setDefaults);
+//Saves the time set by the time picker
+function saveTime() {
+	document.getElementById('status').textContent = 'saving time';
+	var time = document.getElementById('time').value;
+	chrome.storage.sync.set({
+		'time': time
+	}, function() {
+		document.getElementById('status').textContent = 'Options saved';
+		setTimeout(function() {
+			document.getElementById('status').textContent = ''
+		}, 750);
+	});
+}
+
+window.addEventListener('load', setDefaults);
+document.getElementById('submit').addEventListener('click', saveTime);
 
 

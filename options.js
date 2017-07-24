5f9d5b585b45c28ccdef6ffa3aa529fba780ec03
Grey16 <ssyyhh2012@gmail.com>
2017-07-24 17:17:43 -0400

    fiddled around with date and time picker in options

--- options.js
+++ options.js
@@ -1,20 +1,34 @@
 /*This is the script for the options page.
 */
 
-//Sets the date picker to the current date upon page load
 function setDefaults() {
-	document.getElementById('date').valueAsDate = new Date();
-	chrome.storage.sync.get('time', function(items) {
-		document.getElementById('status').textContent = items.time;
+	//Sets the date picker to the current date upon page load
+	document.getElementById('date').valueAsDate = new Date(); 
+	//Prevents user from selecting days prior to today
+	var today = new Date().toISOString().split('T')[0];
+	document.getElementById('date').setAttribute("min", today);
+	//Displays last set time and date below the timepicker
+	chrome.storage.sync.get(['time', 'date'], function(items) {
+		var dateArr = items.date.split('-');
+		var timeArr = items.time.split(':');
+		var local = new Date(dateArr[0], dateArr[1], dateArr[2], timeArr[0], timeArr[1]);
+		var test = Date.UTC(dateArr[0], dateArr[1], dateArr[2], timeArr[0], timeArr[1]);
+		document.getElementById('status').textContent = items.time + " " + items.date + " " + local + " " + local.toUTCString() + " " + test + " " + local.getTime();
 	});
 }
 
-//Saves the time set by the time picker
+//Saves the time and date set by the user
 function saveTime() {
-	document.getElementById('status').textContent = 'saving time';
 	var time = document.getElementById('time').value;
+	var date = document.getElementById('date').value;
+	//Check to see user has set both values
+	if (!time || !date) {
+		return;
+	}
+	//Stores user settings
 	chrome.storage.sync.set({
-		'time': time
+		'time': time,
+		'date': date
 	}, function() {
 		document.getElementById('status').textContent = 'Options saved';
 		setTimeout(function() {
@@ -23,6 +37,7 @@ function saveTime() {
 	});
 }
 
+//Executes functions at appropriate events
 window.addEventListener('load', setDefaults);
 document.getElementById('submit').addEventListener('click', saveTime);
 

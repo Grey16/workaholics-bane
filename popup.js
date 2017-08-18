33388882b5d627798abff138f57bc390c7d11e97
Grey16 <ssyyhh2012@gmail.com>
2017-08-18 08:08:28 -0400

    fixed bug where popup would display NaNh NaNm NaNs when there was no alarm set, started implementation of disable button

--- popup.js
+++ popup.js
@@ -18,32 +18,36 @@ function setup() {
 		document.getElementById('message').textContent = message;
 	});
 	
-	var alarm = true;
-	var scheduledTime;
 	//shows how much time until the alarm goes off
 	chrome.alarms.getAll(function(alarms) {
 		if(!alarms[0]) {
 			document.getElementById('alarms').textContent = "Set Break";
-			alarm = false;
 		} else {
-			scheduledTime = alarms[0].scheduledTime;
+			//creates a countdown that updates itself every second
+			setInterval(function() {
+				var diff = alarms[0].scheduledTime - Date.now();
+				var hours = Math.floor(diff / (1000 * 60 * 60));
+				var minutes = Math.floor((diff % (1000 * 60 * 60)) / (1000 * 60));
+				var seconds = Math.floor((diff % (1000 * 60)) / 1000)
+				var calc = Math.trunc(diff / 1000);
+				calc = Math.trunc(calc / 60);
+				var time = minutes.toString() + "m " + seconds.toString() + "s";
+				if(hours != 0) {
+					time = hours.toString() + "h " + time;
+				}
+				document.getElementById('alarms').textContent = time;
+			}, 1000);
 		}
 	});
 	
-	if(alarm) {
-		setInterval(function() {
-			var diff = scheduledTime - Date.now();
-			var hours = Math.floor(diff / (1000 * 60 * 60));
-			var minutes = Math.floor((diff % (1000 * 60 * 60)) / (1000 * 60));
-			var seconds = Math.floor((diff % (1000 * 60)) / 1000)
-			var calc = Math.trunc(diff / 1000);
-			calc = Math.trunc(calc / 60);
-			var time = minutes.toString() + "m " + seconds.toString() + "s";
-			if(hours != 0) {
-				time = hours.toString() + "h " + time;
-			}
-			document.getElementById('alarms').textContent = time;
-		}, 1000);
+	//if the extension is blocking, create a button that will disable the extension
+	if(document.getElementById('message').textContent == "until break ends") {
+		//creates button and places it under the message
+		var disableBtn = document.createElement("button");
+		var btnText = document.createTextNode("Disable the extension");
+		disableBtn.appendChild(btnText);
+		var place = document.getElementById('message');
+		place.appendChild(disableBtn);
 	}
 	
 }
